f5cbb03fcf96900311739d3908ac08240d5a48b2
Sujin Jang <susan3722@snu.ac.kr>
2022-02-07 14:08:29 +0900

    first commit
    - DBUtil : 모든 DAO에서 공통적으로 사용되는 코드들로 구성됨
    - static{} : driver 연결 / 최초 한번 실행
    - getConnection() : 접속 성공시 Connection 객체 반환 / 접속 실패시 예외(SQLException) 발생
        - throws만 있고 throw는 없는 구조 : 호출한 쪽에서 catch를 해줌
    - close() : select, insert 메소드에 있던 finally 블록을 한꺼번에 처리

---
+++ DBUtil.java
@@ -0,0 +1,76 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBUtil {
+
+
+
+	//최초 무조건 실행, 모두가 공유하는 자원을 한번만 초기화 하는 로직으로 개발
+	static {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//접속 위치 반환, 요청시마다 새로 생성해서 제공함 (공유 안됨)
+	public static Connection getConnection() throws SQLException {
+		String url 
+		= "jdbc:mysql://mydb-1.ckejbqh7xa7q.ap-northeast-2.rds.amazonaws.com:3306/playdata?serverTimezone=UTC";
+		
+		Connection con = DriverManager.getConnection(url, "encore", "playdata");
+		return con;
+	}
+
+	
+	// 모든 DAO 클래스들의 모든 메소드가 반드시 실행되는 자원반환 로직 메소드 : finally 블록 개선
+	/* select : ResultSet -> Statement -> Connection 순서 */
+	public static void close(Connection con, Statement stmt, ResultSet rset) {
+		
+		try {
+			if (rset != null) {
+				rset.close();
+				rset = null;
+			}
+			if (stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+			if (con != null) {
+				con.close();
+				con = null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/* insert, update, delete : Statement -> Connection 순서 */
+	public static void close(Connection con, Statement stmt) {
+		
+		try {
+			if (stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+			if (con != null) {
+				con.close();
+				con = null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+}
+	
